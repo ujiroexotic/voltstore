5dc0c83fd45077098a126a7f51303f0327e7f403
IbnuJabir <kedirjabir12@gmail.com>
2024-11-04 16:34:24 +0300

    chire: config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api-voltstore.up.railway.app",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
