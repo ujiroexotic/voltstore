b66ae95b8ee988c9413545600970d0f3174c3799
IbnuJabir <kedirjabir12@gmail.com>
2024-11-06 03:43:17 +0300

    fix: AuthProvider

--- frontend/app/clientProvider.tsx
+++ frontend/app/clientProvider.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import { CartProvider } from "@/components/CartContext";
+import { AuthProvider } from "@/context/AuthContext";
 export function ClientProvider({
   children,
 }: Readonly<{
@@ -11,11 +12,13 @@ export function ClientProvider({
 }>) {
   return (
     <Provider store={store}>
-      <CartProvider>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </CartProvider>
+      </AuthProvider>
     </Provider>
   );
 }
