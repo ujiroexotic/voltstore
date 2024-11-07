f24232f283b93326179f0fb917c6b168bee75a68
IbnuJabir <kedirjabir12@gmail.com>
2024-11-07 18:02:24 +0300

    chore: check user is admin

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   registerUser,
   loginUser,
@@ -6,39 +6,41 @@ import {
   updateUserProfile,
   deleteUser,
   logoutUser,
-} from '../controllers/userController';
-import { protect } from '../middlewares/authMiddleware';
+} from "../controllers/userController";
+import { protect } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 // Register a new user
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
 // Login a user
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 
 // Logout a user
-router.get('/logout', logoutUser);
+router.get("/logout", logoutUser);
 
 // check if user is admin
-router.get('/checkAdmin', protect, (req, res) => {
-  if (req.user?.role !== 'admin') {
-    return res.status(403).json({ message: 'Access denied: Admins only' });
+router.get("/checkAdmin", protect, (req, res) => {
+  console.log("user:");
+  console.log(req.user);
+  if (req.user?.role !== "admin") {
+    return res.status(403).json({ message: "Access denied: Admins only" });
   }
-  res.status(201).send('Welcome Admin');
+  res.status(201).send("Welcome Admin");
 });
 
-// hell to user 
-router.get('/hello', (req, res) => {
-  res.status(201).send('Hello User');
+// hell to user
+router.get("/hello", (req, res) => {
+  res.status(201).send("Hello User");
 });
 
 // Get user profile (protected)
-router.get('/profile', protect, getUserProfile);
+router.get("/profile", protect, getUserProfile);
 
 // Update user profile (protected)
-router.put('/profile', protect, updateUserProfile);
+router.put("/profile", protect, updateUserProfile);
 
 // Route to delete a user (admin only)
-router.delete('/:userId', protect, deleteUser); // Add route to delete user
+router.delete("/:userId", protect, deleteUser); // Add route to delete user
 export default router;
