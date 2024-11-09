df916431c2beaf6abfeafc7064f7e6494ae8effc
IbnuJabir <kedirjabir12@gmail.com>
2024-11-09 16:31:33 +0300

    chore: secure the route

---
+++ frontend/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // const userId = request.cookies.get("userId")?.value;
+  const authToken = request.cookies.get("authToken")?.value;
+  // console.log("authToken", authToken);
+  // console.log("userId", userId);
+  // If the user is not authenticated, redirect to the login page
+  if (!authToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // Continue to the next middleware or the requested page
+  return NextResponse.next();
+}
+
+// Apply this middleware to specific paths
+export const config = {
+  matcher: ["/cart", "/checkout", "/profile", "/orders"],
+};
