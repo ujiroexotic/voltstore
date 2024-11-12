e31970ecd283e1f826abb3d3692a82e5d95d3ab5
IbnuJabir <kedirjabir12@gmail.com>
2024-11-12 13:37:01 +0300

    feat: add auth middleware for both user and admin token

--- backend/middlewares/authMiddleware.ts
+++ backend/middlewares/authMiddleware.ts
@@ -1,7 +1,7 @@
 //This file contains middleware that protect routes by ensuring only authenticated or authorized users can accesss them.
 import { Request, Response, NextFunction } from "express";
-import jwt from 'jsonwebtoken';
-import User from '../models/user';
+import jwt from "jsonwebtoken";
+import User from "../models/user";
 import asyncHandler from "express-async-handler";
 
 //The AuthRequest allow request to attaching a user property to the request object.
@@ -9,28 +9,41 @@ export interface AuthRequest extends Request {
   user?: any;
 }
 
-export const protect = asyncHandler(async (req: AuthRequest, res: Response, next: NextFunction) => {
-  let token;
-// console.log('User Token:')
-// console.log(req.cookies.authToken)
-  // Look for the token in the cookies
-  if (req.cookies && req.cookies.authToken) {
-    token = req.cookies.authToken;
-    try {
-      const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
-      req.user = await User.findById(decoded.id).select("-password");
-      next();
-    } catch (error) {
-      res.status(401).json({ message: "Not authorized, token failed" });
+export const protect = asyncHandler(
+  async (req: AuthRequest, res: Response, next: NextFunction) => {
+    let token;
+
+    if (req.cookies) {
+      if (req.cookies.authToken) {
+        token = req.cookies.authToken;
+        try {
+          const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+          req.user = await User.findById(decoded.id).select("-password");
+          next();
+        } catch (error) {
+          res.status(401).json({ message: "Not authorized, token failed" });
+        }
+      } else if (req.cookies.userAuth) {
+        token = req.cookies.userAuth;
+        try {
+          const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+          req.user = await User.findById(decoded.id).select("-password");
+          next();
+        } catch (error) {
+          res.status(401).json({ message: "Not authorized, token failed" });
+        }
+      } else {
+        res.status(401).json({ message: "Not authorized, no token" });
+      }
+    } else {
+      res.status(401).json({ message: "Not authorized, no token" });
     }
-  } else {
-    res.status(401).json({ message: "Not authorized, no token" });
   }
-});
+);
 
 export const admin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.user?.role !== 'admin') {
-    return res.status(403).json({ message: 'Access denied: Admins only' });
+  if (req.user?.role !== "admin") {
+    return res.status(403).json({ message: "Access denied: Admins only" });
   }
   next();
 };
