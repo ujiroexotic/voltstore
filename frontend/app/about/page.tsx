0932c227e7dbb8a2d49abe6bf38a7eea91f0c57a
IbnuJabir <kedirjabir12@gmail.com>
2024-11-13 19:58:20 +0300

    style: remove shadows

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className='max-w-3xl p-8 text-center gb-white shadow-lg rounded-lg'>
+      <div className='max-w-3xl p-8 text-center gb-white shadow-sm rounded-lg'>
         <h1 className='text-4xl font-bold text-indigo-600 mb-4'>
           About Us</h1>
 
