406c33d0ea3279488383f2daccb4204e54ddfe1b
agbaniongithub <fiyin.agba@gmail.com>
2024-11-13 09:52:58 -0800

    Page Added: Contact Page

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -65,6 +65,9 @@ function Contact() {
             <div className="mt-8">
               <a href="https://github.com/OrionDooms" target="_blank" className="text-2xl font-bold text-primary">Orion Dooms</a>
             </div>
+            <div className="mt-8">
+              <a href="https://github.com/ujiroexotic" target="_blank" className="text-2xl font-bold text-primary">Ujiro Eruteya</a>
+            </div>
           </div>
 
           <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
