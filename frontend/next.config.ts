f93178bf9c6fe58b49e0916773977c2d06f45266
Ujiro Eruteya <ujiroeruteya@gmail.com>
2025-06-15 15:06:10 +0100

    voltstore

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'export',
   // other config options...
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
 
